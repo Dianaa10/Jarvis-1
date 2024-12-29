9f1ee88c09017a2a68724f0971f7dd4ae5908e1a
Diana <163707717+Dianaa10@users.noreply.github.com>
2024-12-29 01:53:04 -0800

    Add files via upload

---
+++ Jarvis/app-ui/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.text').textillate({
+        loop: false,
+        sync: true,
+        in: {
+            effect: "bounceIn",
+        },
+        out: {
+            effect: "bounceOut",
+        }
+    }); // Remove the comma here
+});
